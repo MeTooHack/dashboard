523a3b59b3da615953cf70fc27409fc23c4bfb67
Pierre Reimertz <pierre.reimertz@gmail.com>
2018-02-01 21:02:58 +0100

    add sponsors

--- modules/poller.js
+++ modules/poller.js
@@ -138,4 +138,3 @@ var Poller = (function() {
     on: addEventListener
   }
 })()
-
---
+++ modules/sponsor-carousel.js
@@ -0,0 +1,12 @@
+
+$('.sponsors').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  centerMode: true,
+  dots: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+  arrows: false,
+  pauseOnHover: false
+});
